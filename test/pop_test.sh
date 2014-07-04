c84b12a1da19102d1c7bb838aed50e0ecdee1e26
Postmodern <postmodern.mod3@gmail.com>
2014-07-03 18:15:49 -0700

    Added a test for --pop

---
+++ test/pop_test.sh
@@ -0,0 +1,21 @@
+. ./test/helper.sh
+
+function setUp()
+{
+	original_path="$PATH"
+	original_gem_home="$GEM_HOME"
+	original_gem_path="$GEM_PATH"
+
+	gem_path --push "$HOME"
+}
+
+function test_pop()
+{
+	gem_path --pop
+
+	assertEquals "did not remove bin/ from \$PATH" "$PATH" "$original_path"
+	assertEquals "did not remove gem dir from \$GEM_PATH" "$GEM_PATH" "$original_gem_path"
+	assertEquals "did not reset \$GEM_HOME" "$GEM_HOME" "$original_gem_home"
+}
+
+SHUNIT_PARENT=$0 . $SHUNIT2
