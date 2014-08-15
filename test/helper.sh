dab48bc92067948fb07575e6fba8ad7a1893ee78
Postmodern <postmodern.mod3@gmail.com>
2014-08-14 19:55:48 -0700

    Don't use RUBY_ROOT.

--- test/helper.sh
+++ test/helper.sh
@@ -5,7 +5,7 @@ export PREFIX="$PWD/test"
 export HOME="$PREFIX/home"
 export PATH="$PWD/bin:$PATH"
 
-eval "$("$RUBY_ROOT/bin/ruby" - <<EOF
+eval "$(ruby - <<EOF
 puts "test_ruby_engine=#{defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'};"
 puts "test_ruby_version=#{RUBY_VERSION};"
 puts "test_ruby_api_version=#{RbConfig::CONFIG['ruby_version']};"
