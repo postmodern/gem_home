e3d10e58ed86c5d981dc7f890578084ec1576488
Postmodern <postmodern.mod3@gmail.com>
2014-08-14 20:59:22 -0700

    Combined the gem_home_push tests together.

--- test/push_test.sh
+++ test/push_test.sh
@@ -1,7 +1,7 @@
 . ./test/helper.sh
 . ./share/gem_home/gem_home.sh
 
-function setUp()
+function test_gem_home_push()
 {
 	dir="$HOME"
 	expected_dir="$dir/.gem/$test_ruby_engine/$test_ruby_version"
@@ -9,23 +9,14 @@ function setUp()
 	original_gem_path="$GEM_PATH"
 
 	gem_home_push "$dir"
-}
 
-function test_gem_home_push_GEM_HOME()
-{
 	assertEquals "did not set \$GEM_HOME correctly" "$expected_dir" \
 		                                        "$GEM_HOME"
-}
 
-function test_gem_home_push_GEM_PATH()
-{
 	assertEquals "did not prepend to \$GEM_PATH" "$expected_dir:$original_gem_path" \
 		                                     "$GEM_PATH"
-}
 
-function test_gem_home_push_PATH()
-{
-	assertEquals "did not prepend bin/ to \$PATH" "$expected_dir/bin:$original_path" \
+	assertEquals "did not prepend bin/ to \$PATH" "$original_path:$expected_dir/bin" \
 	                                              "$PATH"
 }
 
