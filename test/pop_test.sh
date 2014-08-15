b751c9ff5c2ef4b681daefb6f04fd2ab8b6e401e
Postmodern <postmodern.mod3@gmail.com>
2014-08-14 20:04:38 -0700

    Split tests apart.

--- test/pop_test.sh
+++ test/pop_test.sh
@@ -8,18 +8,23 @@ function setUp()
 	original_gem_path="$GEM_PATH"
 
 	gem_home_push "$HOME"
+	gem_home_pop
 }
 
-function test_pop()
+function test_pop_PATH()
 {
-	gem_home_pop
-
 	assertEquals "did not remove bin/ from \$PATH" "$original_path" \
 		                                       "$PATH"
+}
 
+function test_pop_GEM_PATH()
+{
 	assertEquals "did not remove gem dir from \$GEM_PATH" "$original_gem_path" \
 		                                              "$GEM_PATH"
+}
 
+function test_pop_GEM_HOME()
+{
 	assertEquals "did not reset \$GEM_HOME" "$original_gem_home" \
 		                                "$GEM_HOME"
 }
--- test/push_test.sh
+++ test/push_test.sh
@@ -1,21 +1,30 @@
 . ./test/helper.sh
 . ./share/gem_home/gem_home.sh
 
-function test_gem_path_push()
+function setUp()
 {
-	local dir="$HOME"
-	local expected_dir="$dir/.gem/$test_ruby_engine/$test_ruby_version"
-	local original_path="$PATH"
-	local original_gem_path="$GEM_PATH"
+	dir="$HOME"
+	expected_dir="$dir/.gem/$test_ruby_engine/$test_ruby_version"
+	original_path="$PATH"
+	original_gem_path="$GEM_PATH"
 
 	gem_home_push "$dir"
+}
 
+function test_gem_home_push_GEM_HOME()
+{
 	assertEquals "did not set \$GEM_HOME correctly" "$expected_dir" \
 		                                        "$GEM_HOME"
+}
 
+function test_gem_home_push_GEM_PATH()
+{
 	assertEquals "did not prepend to \$GEM_PATH" "$expected_dir:$original_gem_path" \
 		                                     "$GEM_PATH"
+}
 
+function test_gem_home_push_PATH()
+{
 	assertEquals "did not prepend bin/ to \$PATH" "$expected_dir/bin:$original_path" \
 	                                              "$PATH"
 }
