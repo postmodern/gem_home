55d661852ff04860d9a163072f3d6aedaa8a8309
Postmodern <postmodern.mod3@gmail.com>
2014-08-18 07:27:05 -0700

    Update the homebrew formula for 0.1.0.

--- homebrew/gem_home.rb
+++ homebrew/gem_home.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GemHome < Formula
   homepage 'https://github.com/postmodern/gem_home#readme'
-  url 'https://github.com/postmodern/gem_home/archive/v0.0.1.tar.gz'
-  sha1 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+  url 'https://github.com/postmodern/gem_home/archive/v0.1.0.tar.gz'
+  sha1 '338193cbbda0ee7bf0d3c903bb75fd24a4933693'
 
   head 'https://github.com/postmodern/gem_home.git'
 
