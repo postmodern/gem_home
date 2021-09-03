70ec3a3f564be9d1ce5c0ff4c2aeb5feb9b8e481
Postmodern <postmodern.mod3@gmail.com>
2021-09-02 17:59:52 -0700

    Added the desc and license attributes.
    * https://docs.brew.sh/Formula-Cookbook

--- homebrew/gem_home.rb
+++ homebrew/gem_home.rb
@@ -1,9 +1,11 @@
 require 'formula'
 
 class GemHome < Formula
+  desc "Changes your $GEM_HOME"
   homepage 'https://github.com/postmodern/gem_home#readme'
   url 'https://github.com/postmodern/gem_home/archive/v0.1.0.tar.gz'
   sha256 '67e9e174de42de640f144f88cd16b2a315bdec55567467743f8fbb96cba14e4e'
+  license "MIT"
 
   head 'https://github.com/postmodern/gem_home.git'
 
